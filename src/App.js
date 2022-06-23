2973072ea3f1dc33058fc18c82453516e2d54014
Amit <amitmahamuni7@gmail.com>
2022-06-23 12:53:33 +0530

    update file upload process

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useState } from "react";
 
 function App() {
 
-  const [pro, setpro] = useState({})
+  const [file, setFile] = useState({})
   const [drag, setDrag] = useState(false)
 
   function sendData(e) {
     e.preventDefault()
-    Object.entries(pro).map((x, i) => {
+    Object.entries(file).map((x, i) => {
       if (x[1].progress != 100) {
         const formData = new FormData();
         formData.append("docFile", x[1].data);
@@ -16,7 +16,7 @@ function App() {
         axios.post("http://localhost:3001/", formData,
           {
             onUploadProgress: data => {
-              setpro(pre => {
+              setFile(pre => {
                 return {
                   ...pre,
                   [x[0]]: {
@@ -29,7 +29,7 @@ function App() {
           }
         ).then((res) => {
           //console.log(res)
-          setpro(pre => {
+          setFile(pre => {
             return {
               ...pre,
               [x[0]]: {
@@ -44,59 +44,51 @@ function App() {
   }
 
   function selectFile(file) {
-    //console.log(file)
-    let tempfile = Object.values(file)
-    setpro(prev => {
+    setFile(prev => {
       let temp = { ...prev }
-      tempfile.map(x => {
+      Object.values(file).map(x => {
         temp[x.name] = { progress: 0, link: null, preview: URL.createObjectURL(x), data: x }
       })
       return temp;
     })
   }
 
-  function deleteFile(fname, link) {
-    setpro(pre => {
+  function deleteFile(fname) {
+    setFile(pre => {
       let temp = { ...pre };
       delete temp[fname]
       return temp
     })
-
-    if (link) {
-
-    }
   }
 
-  function handleDragIn(e) {
+  function handleDragDrop(e, i) {
     e.preventDefault()
     e.stopPropagation()
-    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
-      setDrag(true)
-    }
-  }
-
-  function handleDrag(e) {
-    e.preventDefault()
-    e.stopPropagation()
-    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
-      setDrag(true)
-    }
-  }
-
-  function handleDragOut(e) {
-    e.preventDefault()
-    e.stopPropagation()
-    setDrag(false)
-  }
-
-  function handleDrop(e) {
-    //console.log(e)
-    e.preventDefault()
-    e.stopPropagation()
-    setDrag(false)
-    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-      selectFile(e.dataTransfer.files)
-      e.dataTransfer.clearData()
+    switch (i) {
+      // handle DragIn
+      case 1:
+        if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+          setDrag(true)
+        }
+        break;
+      // handle Drag
+      case 2:
+        if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+          setDrag(true)
+        }
+        break;
+      // handle DragOut
+      case 3:
+        setDrag(false)
+        break;
+      // handle Drop
+      case 4:
+        setDrag(false)
+        if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+          selectFile(e.dataTransfer.files)
+          e.dataTransfer.clearData()
+        }
+        break;
     }
   }
 
@@ -117,10 +109,10 @@ function App() {
                   <div className="col-md-6 d-flex justify-content-center align-items-center">
                     <div className="text-center w-100 h-100 p-4"
                       style={{ "border": drag ? "3px dashed #000" : null }}
-                      onDragEnter={(e) => handleDragIn(e)}
-                      onDragLeave={(e) => handleDragOut(e)}
-                      onDrop={(e) => handleDrop(e)}
-                      onDragOver={(e) => handleDrag(e)}>
+                      onDragEnter={(e) => handleDragDrop(e, 1)}
+                      onDragOver={(e) => handleDragDrop(e, 2)}
+                      onDragLeave={(e) => handleDragDrop(e, 3)}
+                      onDrop={(e) => handleDragDrop(e, 4)}>
                       <h4>Drop your file to upload</h4>
                       <p>or</p>
                       <label htmlFor="docFile" className="btn btn-primary">Choose your files</label>
@@ -129,10 +121,10 @@ function App() {
                     </div>
                   </div>
                   <div className="col-md-4">
-                    <span>Selected File ({Object.keys(pro).length})</span>
+                    <span>Selected File ({Object.keys(file).length})</span>
                     {
-                      Object.keys(pro).length ?
-                        Object.entries(pro).map((x, i) => {
+                      Object.keys(file).length ?
+                        Object.entries(file).map((x, i) => {
                           return (
                             <div className="card mb-3" key={"fileCard_" + i}>
                               <div className="row g-0">
@@ -146,7 +138,7 @@ function App() {
                                         {x[0]}
                                       </a>
                                       <button type="button" className="btn btn-sm py-0 px-1 ms-1"
-                                        onClick={() => deleteFile(x[0], x[1].link)}>
+                                        onClick={() => deleteFile(x[0])}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
                                           <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
                                         </svg>
